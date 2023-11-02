3e3eb6a07b24789a62dde84510aee5f0c27bbed6
Emilio Morles <emiliomorles>
2023-11-02 11:41:37 -0600

    v.1.1

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-
-# Looping through dictionaries:
-# student_dict_formatted = {name:score for (name, score) in student_dict.items()}
-# print(student_dict_formatted)
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -15,10 +6,16 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # It creates a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 # print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-#  It creates a list of the phonetic code words from a word that the user inputs.
-print(output_list)
 
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        #  It creates a list of the phonetic code words from a word that the user inputs.
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()  # This is the best way to call the input again instead of a while loop.
+    else:
+        print(output_list)
 
-
+generate_phonetic()
